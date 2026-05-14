51a13e6faaaaa5e5d60082a4fd1e8fa67601283c
houssemmakni <houssemmakni73@gmail.com>
2026-05-14 18:17:31 +0000

    fix: correction de la fonctions names avec la bonne pratique de code : TP3

--- exercices/mon_premier_script_avec_fonctions.py
+++ exercices/mon_premier_script_avec_fonctions.py
@@ -1,18 +1,18 @@
-"""
-Count names with more than seven letters
-"""
-def names(prenoms):
-    more_than_seven = 0
-    for prenom in prenoms:
-        if len(prenom) > 7:
-            more_than_seven += 1
-            print(prenom + " est un prénom avec un nombre de lettres supérieur à 7")
-        else:
-            print(prenom + " est un prénom avec un nombre de lettres inférieur ou égal à 7")
-    return more_than_seven
-
-prenoms = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
-print("Nombre de prénoms dont le nombre de lettres est supérieur à 7 : " + str(names(prenoms=prenoms)))
+#"""
+#Count names with more than seven letters
+#"""
+#def names(prenoms):
+#    more_than_seven = 0
+#    for prenom in prenoms:
+#        if len(prenom) > 7:
+#            more_than_seven += 1
+#            print(prenom + " est un prénom avec un nombre de lettres supérieur à 7")
+#        else:
+#            print(prenom + " est un prénom avec un nombre de lettres inférieur ou égal à 7")
+#    return more_than_seven
+#
+#prenoms = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
+#print("Nombre de prénoms dont le nombre de lettres est supérieur à 7 : " + str(names(prenoms=prenoms)))
 
 
 
@@ -21,18 +21,29 @@ print("Nombre de prénoms dont le nombre de lettres est supérieur à 7 : " + st
 
 import unittest
 
-"""
-Count names with more than seven letters
-"""
-def names(prenoms):
-    more_than_seven = 0
+MAX_SHORT_NAME_LENGTH = 7
+
+
+def names(prenoms: list[str]) -> int:
+    """
+    Count names with more than seven letters
+    """
+    long_names_count = 0
+
     for prenom in prenoms:
-        if len(prenom) > 7:
-            more_than_seven += 1
-            print(prenom + " est un prénom avec un nombre de lettres supérieur à 7")
+        if has_more_letters_than_limit(prenom):
+            long_names_count += 1
+            print(f"{prenom} est un prénom avec plus de {MAX_SHORT_NAME_LENGTH} lettres")
         else:
-            print(prenom + " est un prénom avec un nombre de lettres inférieur ou égal à 7")
-    return more_than_seven
+            print(f"{prenom} est un prénom avec {MAX_SHORT_NAME_LENGTH} lettres ou moins")
+
+    return long_names_count
+
+
+def has_more_letters_than_limit(prenom: str) -> bool:
+    return len(prenom) > MAX_SHORT_NAME_LENGTH
+
+
 
 class TestNamesMethod(unittest.TestCase):
      def test_names(self):
